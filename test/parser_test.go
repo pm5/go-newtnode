1d028adbfe96962771efdd9caf49d9c0a4b0cda9
Pomin Wu <pomin5@gmail.com>
2015-02-26 23:36:17 +0800

    fix tests

--- test/parser_test.go
+++ test/parser_test.go
@@ -9,7 +9,7 @@ func TestASTNodeRegexPrint(t *testing.T) {
 	n := &gopc.ASTNode{Type: "regex", Len: 0, Pos: 0}
 	expected := "regex"
 	if n.String() != expected {
-		t.Errorf("regex node string format, got `%s`, expected `%s`", n, expected)
+		t.Fatalf("regex node string format, got `%s`, expected `%s`", n, expected)
 	}
 }
 
@@ -17,7 +17,7 @@ func TestASTNodeCharPrint(t *testing.T) {
 	n := &gopc.ASTNode{Type: "char", Len: 1, Pos: 13, Content: "c"}
 	expected := "char:1:13 'c'"
 	if n.String() != expected {
-		t.Errorf("char node string format, got `%s`, expected `%s`", n, expected)
+		t.Fatalf("char node string format, got `%s`, expected `%s`", n, expected)
 	}
 }
 
@@ -36,7 +36,7 @@ func TestASTNodeTagPrint(t *testing.T) {
   regex 'hello, world!'
   char:1:14 '"'`
 	if n.String() != expected {
-		t.Errorf("char node string format, got `%s`, expected `%s`", n, expected)
+		t.Fatalf("char node string format, got `%s`, expected `%s`", n, expected)
 	}
 }
 

52360f5ea7e1ba1f1f2d0a03842759c42ee34f9e
Pomin Wu <pomin5@gmail.com>
2015-03-06 09:58:51 +0800

    add char parser

--- parser.go
+++ parser.go
@@ -1,8 +1,20 @@
 package newtnode
 
 type Parser struct {
+	Type    string
+	Content string
 }
 
-func NewParser() *Parser {
-	return &Parser{}
+func NewParserChar(content string) *Parser {
+	return &Parser{Type: "char", Content: content}
+}
+
+func (p *Parser) Parse(content string) (*Node, error) {
+	switch p.Type {
+	case "char":
+		if content == p.Content {
+			return NewNodeChar(content, len(content), 0), nil
+		}
+	}
+	return nil, nil
 }
--- test/parser_test.go
+++ test/parser_test.go
@@ -5,9 +5,29 @@ import (
 	"testing"
 )
 
-func TestNewParser(t *testing.T) {
-	p := newtnode.NewParser()
+func TestNewParserChar(t *testing.T) {
+	p := newtnode.NewParserChar(`"`)
 	if p == nil {
 		t.Fatalf("Parser creation failed")
+	}
+}
+
+func TestNewParserCharParse(t *testing.T) {
+	p := newtnode.NewParserChar(`"`)
+	n, err := p.Parse(`"`)
+	if n == nil || err != nil {
+		t.Fatalf("Parser char parse failed")
+	}
+	if n.Type != "char" {
+		t.Fatalf("Parser char parse failed. Expected `char`, got `%s`", n.Type)
+	}
+	if n.Content != `"` {
+		t.Fatalf("Parser char parse failed. Expected `\"`, got `%s`", n.Content)
+	}
+	if n.Len != 1 {
+		t.Fatalf("Parser char parse failed. Expected `1`, got `%d`", n.Len)
+	}
+	if n.Pos != 0 {
+		t.Fatalf("Parser char parse failed. Expected `1`, got `%d`", n.Pos)
 	}
 }

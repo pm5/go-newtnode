ee2d1603c0c78eca0750f347c1b064c4cfd79695
Pomin Wu <pomin5@gmail.com>
2015-03-27 22:17:36 +0800

    working on lang parser

--- lang.go
+++ lang.go
@@ -2,6 +2,14 @@ package spc
 
 func newLangParser() (lang *TagParser) {
 	lang = NewTagParser("lang")
+	rule := NewTagParser("rule")
+	name := NewRegexpParser(`[a-zA-Z\-_]+`)
+	rule.Add(name, false)
+	rule.Add(NewCharParser(`:`), false)
+	expr := NewTagParser("expr")
+	rule.Add(expr, false)
+	rule.Add(NewCharParser(`;`), false)
+	lang.Add(rule, true)
 	return
 }
 
